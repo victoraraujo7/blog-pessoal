236425dcfebb508ef1fb714f4c7a39364af22d8f
Victor <victor.araujo07@hotmail.com>
2021-03-03 10:20:19 -0300

    Criado Repository Contato

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/repository/ContatoRepository.java
@@ -0,0 +1,8 @@
+package org.generation.blogPessoal.repository;
+
+import org.generation.blogPessoal.model.Contato;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContatoRepository extends JpaRepository<Contato, Long> {
+
+}
