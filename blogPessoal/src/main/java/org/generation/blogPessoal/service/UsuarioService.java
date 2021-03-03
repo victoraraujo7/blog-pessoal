8b710d36eb1699872a5b934e64d7b34971a9c606
Victor <victor.araujo07@hotmail.com>
2021-03-03 10:40:03 -0300

    Atualização security de cadastro com validação de bloqueio de cadastro

--- blogPessoal/src/main/java/org/generation/blogPessoal/service/UsuarioService.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/service/UsuarioService.java
@@ -16,11 +16,14 @@ public class UsuarioService {
 
 	@Autowired
 	private UsuarioRepository repository;
-	
 
 	// criar um metodo
 
 	public Usuario CadastrarUsuario(Usuario usuario) {
+
+		if (repository.findByUsuario(usuario.getUsuario()).isPresent())
+			return null;
+
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
 		String senhaEncoder = encoder.encode(usuario.getSenha());
@@ -39,10 +42,10 @@ public class UsuarioService {
 				String auth = user.get().getUsuario() + ":" + user.get().getSenha();
 				byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
 				String authHeader = "Basic " + new String(encodedAuth);
-				
+
 				user.get().setToken(authHeader);
 				user.get().setNome(usuario.get().getNome());
-				
+
 				return user;
 			}
 		}
