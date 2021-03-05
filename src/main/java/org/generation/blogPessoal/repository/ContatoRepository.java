2acd13373cd9de27e4e173b1709faabc041d394e
Victor <victor.araujo07@hotmail.com>
2021-03-05 09:42:58 -0300

    Criado package e class SwaggerConfig

--- src/main/java/org/generation/blogPessoal/repository/ContatoRepository.java
+++
@@ -1,8 +0,0 @@
-package org.generation.blogPessoal.repository;
-
-import org.generation.blogPessoal.model.Contato;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContatoRepository extends JpaRepository<Contato, Long> {
-
-}
